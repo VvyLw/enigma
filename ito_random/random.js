d7167e138f8517e71e4a6002667874cd9bbe0718
VvyLw <owl1465fu9@gmail.com>
2024-02-02 13:43:52 +0900

    modify

--- ito_random/random.js
+++ ito_random/random.js
@@ -2,24 +2,19 @@
 "use strict";
 let id;
 let rand;
+
 const generate = () => {
     const people = document.getElementById('num').value;
-    if(isNaN(people)) {
+    if (isNaN(people)) {
         console.error("入力された値が数字ではありません。");
         return;
     }
     const num = [];
-    const res = [];
-    for(let i = 0; ++i <= 100;) {
+    rand = [];
+    for (let i = 0; ++i <= 100;) {
         num.push(i);
     }
-    for(let i = 0; i < parseInt(people); ++i) {
-        const id = Math.floor(Math.random() * num.length);
-        res.push(num[id]);
-        num.splice(id, 1);
-    }
-    rand = [];
-    for(let i = 0; i < parseInt(people); ++i) {
+    for (let i = 0; i < parseInt(people); ++i) {
         const j = Math.floor(Math.random() * num.length);
         rand.push(num[j]);
         num.splice(j, 1);
@@ -30,8 +25,8 @@ const generate = () => {
 
 const show = () => {
     const htmlRand = document.getElementById('result');
-    htmlRand.innerHTML = `あなたの数字はこちら: ${rand[currentIndex]}<br>確認できたら「確認ボタン」を押し、次の人へ渡してください`;
-    if(id < rand.length - 1) {
+    htmlRand.innerHTML = `あなたの数字はこちら: ${rand[id]}<br>確認できたら「確認ボタン」を押し、次の人へ渡してください`;
+    if (id < rand.length - 1) {
         document.getElementById('confirm').style.display = 'block';
     } else {
         document.getElementById('confirm').style.display = 'none';
